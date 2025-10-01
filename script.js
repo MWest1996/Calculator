970947b3f6c4f3c64359e9a2e9638bf4e8bd9328
Marvin West <marvin.a.west@gmail.com>
2025-09-30 20:02:48 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   console.log("DOM fully loaded and parsed");
+// });
+
+const calculator = document.querySelector(`.calculator`)
+const keys = calculator.querySelector(`.keys`) 
+const display = document.querySelector(`.display`)
+
+//checks for key press events and matches the key pressed to the appropriate button
+keys.addEventListener(`click`, e => {
+    if (e.target.matches(`button`)) {
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+        const previousKeyType = calculator.dataset.previousKeyType
+        
+
+        if (!action) {
+            if (displayedNum === "0" || previousKeyType === `operator`) {
+                display.textContent = keyContent
+            } else {
+                display.textContent = displayedNum + keyContent
+            } // displayedNum checks the value shown in the display item in the HTML. IF it's 0, then it just shows that number.
+            // If a button is pressed, said number is added to the html element
+        }
+
+        if (action === `decimal`) {
+            display.textContent = displayedNum + `.`
+        }
+
+        if (
+            action === `add` ||
+            action === `subtract` ||
+            action === `multiply` ||
+            action === `divide`
+        ) {
+            key.classList.add(`is-depressed`)
+
+            calculator.dataset.previousKeyType = `operator`
+            calculator.dataset.firstValue = displayedNum
+            calculator.dataset.operator = action
+            Array.from(key.parentNode.children).forEach(k =>
+            k.classList.remove(`is-depressed`),
+        )
+        }
+
+        if (action === `calculate`) {
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayedNum
+
+            const calculate = (n1, operator, n2) => {
+                let result = ''
+
+                if (operator === `add`) {
+                    result = parseFloat(n1) + parseFloat(n2)
+                } else if (operator === `subtract`) {
+                    result = parseFloat(n1) - parseFloat(n2)
+                } else if (operator === `multiply`) {
+                    result = parseFloat(n1) * parseFloat(n2)
+                } else if (operator ===`divide`) {
+                    result = parseFloat(n1) / parseFloat(n2)
+                }
+
+                return rersult
+            }
+
+            display.textContent = calculate(firstValue, operator, secondValue)
+        }
+
+
+        // if (!action) {
+        //     console.log(`Number Key!`)
+        // }
+        // if (
+        //     action === `add` ||
+        //     action === `subtract` ||
+        //     action === `multiply` ||
+        //     action === `divide`
+        // ) {
+        //     console.log(`Operator key!`)
+        // }
+
+        // if (action === `decimal`){
+        //     console.log(`Decimal Key!`)
+        // }
+        // if (action === `clear`){
+        //     console.log(`Clear Key!`)
+        // }
+        // if (action === `calculate`){
+        //     console.log(`Equal Key!`)
+        // }
+    }
+});
+
